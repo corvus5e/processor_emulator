8bfdec276eca0eb47476b37d97bf6dc02c08371d
corvus5e <corvus5e@gmail.com>
2025-12-15 00:24:20 +0100

    Fix sample program

--- asm_samples/sample.asm
+++ asm_samples/sample.asm
@@ -1,12 +1,13 @@
 # Simple program that calculates 5 * 4
-load r0 5
-load r1 4
+load r0 5 #summand
+load r1 4 #counter
+load r2 0 #result
 loop:
-sub r1 1 # We already have r0 * 1
 jnz r1 continue 
 jmp return
 continue:
-add r0 r0
+add r2 r0
+sub r1 1
 jmp loop
 return:
 exit
